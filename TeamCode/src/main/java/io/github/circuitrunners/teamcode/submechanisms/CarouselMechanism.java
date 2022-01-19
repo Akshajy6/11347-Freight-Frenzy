ff0ebbbb7451a4c80d6744a524aebeffdcb42130
vijayshastri <vijay.shastri@circuitrunners.com>
2022-01-19 16:39:54 -0500

    QOL changes
    Now you can slow the robot down while driving for those precise maneuvers. Also, there's a base autonomous class that automatically sets up all the submechanisms for immediate use. Finally, there's a drive submechanism method that lets you apply a certain set of powers to a motor for a certain amount of time.

--- TeamCode/src/main/java/io/github/circuitrunners/teamcode/submechanisms/CarouselMechanism.java
+++
@@ -1,7 +0,0 @@
-package io.github.circuitrunners.teamcode.submechanisms;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-public class CarouselMechanism {
-    private DcMotor
-}
--- TeamCode/src/main/java/io/github/circuitrunners/teamcode/submechanisms/DriveMechanism.java
+++ TeamCode/src/main/java/io/github/circuitrunners/teamcode/submechanisms/DriveMechanism.java
@@ -1,9 +1,13 @@
 package io.github.circuitrunners.teamcode.submechanisms;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import static android.os.SystemClock.sleep;
 
 public class DriveMechanism {
     private final double MAX_POWER = 0.8;
+    private final double DEADZONE = 0.1;
 
     private DcMotor[] motors = new DcMotor[4];
 
@@ -12,14 +16,25 @@ public class DriveMechanism {
         motors[1] = frontRight;
         motors[2] = backLeft;
         motors[3] = backRight;
+
+        // motors[0].setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void drive(double leftPower, double rightPower) {
+        if (Math.abs(leftPower) < DEADZONE) leftPower = 0;
+        if (Math.abs(rightPower) < DEADZONE) rightPower = 0;
+
         leftPower *= MAX_POWER;
         rightPower *= MAX_POWER;
 
         for(int i = 0; i < motors.length; i++) {
             motors[i].setPower((i % 2 == 0) ? leftPower : rightPower);
         }
+    }
+
+    public void driveTimed(double leftPower, double rightPower, int millis) {
+        drive(leftPower, rightPower);
+        sleep(millis);
+        drive(0, 0);
     }
 }
